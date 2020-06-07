e8ca92e2b2825e6faecdb79e2e0baf9b5c19387b
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-07 17:01:59 +0300

    add POJO

---
+++ jms-example/src/main/java/com/JmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsServiceApplication.class, args);
+	}
+
+}
---
+++ jms-example/src/main/java/com/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 3377601297167263303L;
+
+    private UUID id;
+    private String message;
+}
